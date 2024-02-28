61d3da333eb99834ca5bfb802e0280deee367796
PauloPalauro <paulopalauro@gmail.com>
2024-02-28 17:15:31 -0300

    Product Add & View

--- e-commerce/src/main/java/com/palauro/ecommerce/controller/AdmimController.java
+++ e-commerce/src/main/java/com/palauro/ecommerce/controller/AdmimController.java
@@ -1,5 +1,9 @@
 package com.palauro.ecommerce.controller;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,8 +12,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
 import com.palauro.ecommerce.dto.ProductDTO;
 import com.palauro.ecommerce.model.Category;
+import com.palauro.ecommerce.model.Product;
 import com.palauro.ecommerce.service.CategoryService;
 import com.palauro.ecommerce.service.ProductService;
 
@@ -76,6 +84,33 @@ public class AdmimController {
         model.addAttribute("productDTO", new ProductDTO());
         model.addAttribute("categories", categoryService.getAllCategory());
         return "productsAdd";
+    }
+
+    @PostMapping("/admin/products/add")
+    public String productAddPost(@ModelAttribute("productDTO") ProductDTO productDTO,
+                                 @RequestParam("productImage") MultipartFile file,
+                                 @RequestParam("imgName") String imgName) throws IOException {
+
+        Product product = new Product();
+        product.setId(productDTO.getId());
+        product.setName(productDTO.getName());
+        product.setCategory(categoryService.getCategoryById(productDTO.getCategoryId()).get());
+        product.setPrice(productDTO.getPrice());
+        product.setWeight(productDTO.getWeight());
+        product.setDescription(productDTO.getDescription());
+        String imageUUID;
+        if(!file.isEmpty()){
+            imageUUID =  file.getOriginalFilename();
+            Path fileNameAndPAth = Paths.get(uploadDir, imageUUID);
+            Files.write(fileNameAndPAth, file.getBytes());
+        } else{
+            imageUUID = imgName;
+            
+        }
+        product.setImageName(imageUUID);
+        productService.addProduct(product);
+
+        return "redirect:/admin/products";
     }
 
 }
--- e-commerce/src/main/java/com/palauro/ecommerce/service/ProductService.java
+++ e-commerce/src/main/java/com/palauro/ecommerce/service/ProductService.java
@@ -1,6 +1,8 @@
 package com.palauro.ecommerce.service;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.palauro.ecommerce.model.Product;
@@ -17,6 +19,23 @@ public class ProductService {
     public List<Product> getAllProducts(){
         return productRepository.findAll();
     }
+
+    public void addProduct(Product product){
+        productRepository.save(product);
+    }
+
+    public void removeProductById(long id){
+        productRepository.deleteById(id);
+    }
+
+    public Optional<Product> getProductById(long id){
+        return productRepository.findById(id);
+    }
+
+    public List<Product> getAllProductsByCategoryId(int id){
+        return productRepository.findAllByCategory_Id(id);
+    }
+
 
 
 }

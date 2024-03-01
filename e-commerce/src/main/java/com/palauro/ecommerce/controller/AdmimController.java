9448ebd81d3475b20f8d0de50065b67ccf25f7d1
PauloEduardoPalauro <paulopalauro@gmail.com>
2024-03-01 10:06:41 -0300

    Authentication - Creating Models

--- e-commerce/src/main/java/com/palauro/ecommerce/controller/AdmimController.java
+++ e-commerce/src/main/java/com/palauro/ecommerce/controller/AdmimController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -69,7 +70,6 @@ public class AdmimController {
         } else {
             return "404";
         }
-
     }
 
     // Product Section
